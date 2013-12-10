f681c9dd6e079cbf37e876daf976457116eaf2e7
ekoon <aerospike1111@gmail.com>
2013-12-09 23:05:48 -0500

    split the data so that training and testing can be used

--- MainScript.m
+++ MainScript.m
@@ -17,9 +17,42 @@ function[] = MainScript ( dataset ) % dataset
     %Output type is stored on the last column
     data = csvread(csv_data);
     
-    [row column] = size(data)
+    [row column] = size(data);
+    [x y] = hist(data(:,column));
     
-
+    %count the number of different outputs
+    num_of_output = 0;
+    for i = 1 : numel(x)
+        if x(i)>0
+            num_of_output = num_of_output +1;
+        end
+    end
+    
+    %Create an index table
+    %first row is the output type
+    %second row is counter
+    %third and above is the index
+    array_output_type = zeros(1, num_of_output);
+    fill = 0;
+    for i = 1 : row
+        element = data(i, column);
+        member = ismember(array_output_type(1,:),element);
+        ind = find(member>0);
+        %new output goes to its own column
+        if isempty(ind)
+            fill = fill + 1;
+            array_output_type(1,fill) = element;
+            array_output_type(2,fill) = 1;
+            array_output_type(3,fill) = i;
+        %old output goes under its output type
+        else
+            array_output_type(2,ind) = array_output_type(2,ind) + 1;
+            array_output_type(array_output_type(2,ind)+2,ind) = i;
+        end
+    end
+    
+    %half the size of minimal occurance is used for testing
+    minimal_test_size = min(array_output_type(2,:))/2;
     
     
 end
